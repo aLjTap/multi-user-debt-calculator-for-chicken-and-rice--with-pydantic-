5c02149a13b1083e75de877621e3216c15e48372
sozefvs <104849681+sozefvs@users.noreply.github.com>
2023-07-08 14:25:02 +0300

    env_fix

---
+++ data__mine.py
@@ -0,0 +1,310 @@
+import requests
+import pydantic
+from typing import Optional
+import string
+import typing as t
+from datetime import datetime
+import json
+import pprint
+import os
+from dotenv import load_dotenv
+load_dotenv()
+FILENAME = "chicken.json"
+
+
+class User(pydantic.BaseModel):
+    username: str
+    password: str
+    # command + / all delete command
+
+    @pydantic.validator("username")
+    def username_valid(cls, value):
+        if any(item in value for item in string.punctuation):
+            raise ValueError("try again")
+        else:
+
+            return value
+
+    @pydantic.root_validator()
+    def check_password(cls, values):
+
+        if os.getenv(values.get("username")) != values.get("password"):
+
+            raise ValueError("Your password is incorrect")
+        else:
+            print("login succesful")
+            return values
+
+
+class Eat(pydantic.BaseModel):
+
+    username: list
+    eat_chicken: list
+    eat_rice: list
+    total_chicken: float
+    total_rice: float
+    date: datetime = datetime.utcnow()
+    # TODO: BURDA BUG VAR  OR KISMI HATALI FAZLADAN İF EKLNECEK (NEGATİF KONTROL)
+
+    @pydantic.root_validator()
+    def check_values(cls, values):
+        eat_rice = values.get("eat_rice")
+        eat_chicken = values.get("eat_chicken")
+
+        for rice in eat_rice:
+            if rice < 0.0:
+                raise ValueError("You cannot enter a negative value for rice.")
+            elif rice == 0.0 and all(chicken == 0.0 for chicken in eat_chicken):
+                raise ValueError("Both rice and chicken cannot be zero.")
+
+        for chicken in eat_chicken:
+            if chicken < 0.0:
+                raise ValueError(
+                    "You cannot enter a negative value for chicken.")
+
+        return values
+
+
+class Buy(pydantic.BaseModel):
+
+    username: str
+    buy_rice: float
+    buy_chicken: float
+    eaten_chicken: float
+    eaten_rice: float
+    per_chicken: float
+    per_rice: float
+    date: datetime = datetime.utcnow()
+
+    @pydantic.root_validator()
+    def check_values(cls, values):
+        if (float(values.get("buy_rice")) > 0.0 and float(values.get("buy_chicken")) >= 0.0) or (float(values.get("buy_rice")) >= 0.0 and float(values.get("buy_chicken")) > 0.0):
+            return values
+        else:
+            raise ValueError(
+                "You cannot enter a negative value or double zero!!!!")
+
+
+class Debt(pydantic.BaseModel):
+    buyer: str
+    creditor: str
+    amount: float
+    date: Optional[datetime] = datetime.utcnow()
+
+
+class Database(pydantic.BaseModel):
+    buy: t.List[Buy]
+    eat: t.List[Eat]
+    debt: t.List[Debt]
+    version = "1.0"
+
+
+def write_database(database: Database):
+    with open(FILENAME, "w") as f:
+        f.write(database.json(indent=2))
+
+
+def read_database():
+    try:
+        with open(FILENAME, "r") as f:
+            if not (data := f.read()):
+                return None
+            database = pydantic.parse_raw_as(Database, data)
+
+        return database
+    except FileNotFoundError:
+        print("Error : File not found !!! ")
+
+
+def total(db: Database):
+    total_chicken = 0
+    total_rice = 0
+    for buy in db.buy:
+        total_chicken += buy.buy_chicken
+        total_rice += buy.buy_rice
+    for eat in db.eat:
+        total_chicken -= eat.total_chicken
+        total_rice -= eat.total_rice
+    return total_chicken, total_rice
+
+
+def user_buy_history(user: User, all: t.List["Buy"]):
+    user_special: t.List["Buy"] = []
+    for buy in all:
+        if buy.username == user.username:
+            user_special.append(buy)
+    return user_special
+
+
+# def user_all_buy_history(all: t.List["Buy"]):
+#     user_special: t.List["Buy"] = []
+#     for buy in all:
+#         user_special.append(buy)
+#     return user_special
+
+
+# TODO: İLKERİN aldığı TAVUK BİTTİ
+
+# NOTE: True -> Chicken
+# NOTE: False -> Rice
+
+
+def algorithm_of_chicken(db: Database):
+
+    id = find_chicken(db)
+    chicken_debt = []
+    total_eat_chicken = db.eat[-1].total_chicken
+    for fucking in range(len(list(item.username for item in db.buy))):
+        rem_chicken = (db.buy[id].buy_chicken - db.buy[id].eaten_chicken)
+        if total_eat_chicken <= rem_chicken:
+            procces_amount = total_eat_chicken
+            total_eat_chicken -= rem_chicken
+            chicken_debt.append(debt_proccesser(db, procces_amount, id, True))
+        if total_eat_chicken > rem_chicken:
+            procces_amount = rem_chicken
+            total_eat_chicken -= rem_chicken
+            chicken_debt.append(debt_proccesser(db, procces_amount, id, True))
+            id += 1
+        if total_eat_chicken <= 0:
+            break
+
+    return chicken_debt, id, total_eat_chicken
+
+
+def algorithm_of_rice(db: Database):
+    id = find_rice(db)
+    print(id)
+    rice_debt = []
+    total_eat_rice = db.eat[-1].total_rice
+    for fucking in range(len(list(item.username for item in db.buy))):
+        rem_rice = (db.buy[id].buy_rice - db.buy[id].eaten_rice)
+        if total_eat_rice <= rem_rice:
+            procces_amount = total_eat_rice
+            total_eat_rice -= rem_rice
+            rice_debt.append(debt_proccesser(db, procces_amount, id, False))
+        if total_eat_rice > rem_rice:
+            procces_amount = rem_rice
+            total_eat_rice -= rem_rice
+            rice_debt.append(debt_proccesser(db, procces_amount, id, False))
+            id += 1
+        if total_eat_rice <= 0:
+            break
+
+    return rice_debt, id, total_eat_rice
+
+#TODO:SAME FİND RİCE
+def find_chicken(db: Database):
+    id = 0
+    for i in db.buy:
+        if i.buy_chicken != i.eaten_chicken:
+            break
+        else:
+            id += 1
+
+    return id
+
+
+def find_rice(db: Database):
+    id = 0
+    _, rice = total(db)
+    if rice == 0:
+        return id
+    for i in db.buy:
+        print(i)
+        if i.buy_rice != i.eaten_rice and not (i.buy_rice == 0 and i.eaten_rice == 0):
+            break
+        else:
+            id += 1
+
+    return id
+
+
+def debt_proccesser(db: Database, procces_amount: float, id: int, chicken_or_rice: bool):
+    chicken_or_rice__debt = []
+    for i in range(len(db.eat[-1].eat_chicken)):
+        if chicken_or_rice is True and db.eat[-1].total_chicken != 0:
+            chicken_or_rice__debt.append(float(
+                (db.eat[-1].eat_chicken[i] / db.eat[-1].total_chicken) * (db.buy[id].per_chicken * procces_amount)))
+        if chicken_or_rice is False and db.eat[-1].total_rice != 0:
+            chicken_or_rice__debt.append(float(
+                (db.eat[-1].eat_rice[i] / db.eat[-1].total_rice) * (db.buy[id].per_rice * procces_amount)))
+
+    return chicken_or_rice__debt
+
+
+def debt_printer(db: Database, chicken_or_rice_debt_list: list, id: int, rice_or_chicken: bool):
+    item = 0
+
+    while item != len(chicken_or_rice_debt_list[0]):
+        debt_list = debt_user_elements(chicken_or_rice_debt_list, item)
+        if rice_or_chicken is True:
+            i = find_chicken(db)
+        else:
+            i = find_rice(db)
+        for username in range(len(debt_list)):
+            if item < len(db.eat[-1].username) and db.buy[i].username != db.eat[-1].username[item]:
+                debt = Debt(
+                    buyer=db.buy[i].username,
+                    creditor=db.eat[-1].username[item],
+                    amount=debt_list[username]
+                )
+
+                db.debt.append(debt)
+
+            if i != id:
+                i += 1
+        item += 1
+    return db
+
+
+# NOTE:CHANGE SUM ELEMENTS
+def debt_user_elements(lst: list, i: int):
+    total = []
+    for sublist in lst:
+        if i < len(sublist):
+            total.append(sublist[i])
+    return total
+
+
+def eaten_chicken_printer(db: Database, id: int, total_eat_chicken: float):
+    i = find_chicken(db)
+    while i != id:
+        db.buy[i].eaten_chicken = db.buy[i].buy_chicken
+        i += 1
+    if id == i:
+        db.buy[i].eaten_chicken = db.buy[i].buy_chicken + total_eat_chicken
+    return db
+
+
+def eaten_rice_printer(db: Database, id: int, total_eat_rice: float):
+    i = find_rice(db)
+    while i != id:
+        db.buy[i].eaten_rice = db.buy[i].buy_rice
+        i += 1
+    if id == i:
+        db.buy[i].eaten_rice = db.buy[i].buy_rice + total_eat_rice
+    return db
+
+
+def pay_off(db: Database, username: str, buyer: str, amount: float):
+    debt = Debt(
+        buyer=buyer,
+        creditor=username,
+        amount=-1 * (amount)
+    )
+    db.debt.append(debt)
+    return db
+
+#TODO: KARŞIKLIKLI BORÇLARI TOPLA
+def sum_debts(db: Database):
+    debt_totals = {}
+    for debt in db.debt:
+        buyer = debt.buyer
+        creditor = debt.creditor
+        amount = debt.amount
+        key = f"{buyer}-{creditor}"
+        if key in debt_totals:
+            debt_totals[key] += amount
+        else:
+            debt_totals[key] = amount
+    return debt_totals
---
+++ front_chicken_pro.py
@@ -0,0 +1,251 @@
+from data__mine import *
+import math
+import requests
+FILENMAE = "chicken.json"
+
+
+any_user = input("Your username:\n")
+any_password = input("\nEnter your password\n")
+user = User(username=any_user, password=any_password)
+Methods = {
+    1: "eat",
+    2: "buy",
+    3: "see purchase history",
+    4: "see eat history",
+    5: "payy of bitch",
+    6: "exit"
+}
+Users = {
+    1: "Mehmet",
+    2: "Ali",
+    3: "Nasir",
+    4: "ilker",
+    5: "Ferhat",
+    6: "exit"
+
+}
+
+db = read_database()
+if not db:
+    db = Database(buy=[], eat=[], debt=[])
+history = user_buy_history(user, db.buy)
+if history:
+    _ago = None
+else:
+    _ago = round((datetime.utcnow() - history[-1].date).total_seconds()/3600)
+
+chicken, rice = total(db)
+print(f"hello , we have {chicken} gr chicken and {rice} gr rice")
+for method in Methods:
+    print(f"{method} - {Methods[method]}")
+any_choose = input("Waiting for your input:...\n")
+any_username = []
+eating_rice = []
+eating_chicken = []
+username = ""
+user_number = 0
+if any_choose == "1":
+    while username != Users[6]:
+        if user_number != 0:
+            del Users[user_number]
+        for user in Users:
+            print(f"{user} - {Users[user]}")
+        user_number = int(input("Who is eating?:...\n"))
+        username = Users[user_number]
+
+        if username != Users[6]:
+
+            any_username.append(username)
+
+            chicken_amount = float(
+                input("How much chicken do you want to eat?:\n"))
+            rice_amount = float(input("How much rice do you want to eat?:\n"))
+            eating_rice.append(rice_amount)
+            eating_chicken.append(chicken_amount)
+
+    sum_rice = sum(eating_rice)
+    sum_chicken = sum(eating_chicken)
+    eat = Eat(
+
+
+        username=any_username,
+        eat_chicken=eating_chicken,
+        eat_rice=eating_rice,
+        total_chicken=sum_chicken,
+        total_rice=sum_rice
+
+    )
+
+    try:
+        if chicken >= sum_chicken and rice >= sum_rice:
+            db.eat.append(eat)
+            chicken_debt, id_chicken, total_eat_chicken = algorithm_of_chicken(
+                db)
+            rice_debt, id_rice, total_eat_rice = algorithm_of_rice(db)
+
+            db = debt_printer(db, chicken_debt, id_chicken, True)
+            db = debt_printer(db, rice_debt, id_rice, False)
+            db = eaten_chicken_printer(db, id_chicken, total_eat_chicken)
+            db = eaten_rice_printer(db, id_rice, total_eat_rice)
+            write_database(db)
+            print("\nEnjoy your meal\n")
+
+        else:
+            raise Exception("Not enough chickens or rice")
+    except Exception as e:
+        raise
+
+
+if any_choose == "2":
+
+    try:
+        if _ago is None or _ago >= 3:
+            buying_rice = float(input("How much rice do you want to buy?:\n"))
+            if buying_rice != 0.0:
+                price_rice = float(input("how much did you get the rice\n"))
+            else:
+                price_rice = 0
+                buying_rice = 1
+            buying_chicken = float(
+                input("How much chicken do you want to buy?:\n"))
+            if buying_chicken != 0.0:
+                price_chicken = float(
+                    input("how much did you get the chicken\n"))
+            else:
+                price_chicken = 0
+                buying_chicken = 1
+            buy = Buy(
+                username=any_user,
+                buy_rice=(0 if buying_rice == 1 else buying_rice),
+                buy_chicken=(0 if buying_chicken == 1 else buying_chicken),
+                eaten_chicken=0.0,
+                eaten_rice=0.0,
+                per_chicken=price_chicken / buying_chicken,
+                per_rice=price_rice / buying_rice,
+            )
+
+            db.buy.append(buy)
+            write_database(db)
+        else:
+            raise Exception("you already shopped in 3 hours")
+    except Exception as e:
+        print("Error:", str(e))
+
+if any_choose == "3":
+
+    if _ago == 0:
+        print((datetime.utcnow() - history[-1].date).total_seconds()/60)
+        print(
+            f"your last purchase {history[-1].buy_chicken} gr  chicken {int((datetime.utcnow() - history[-1].date).total_seconds()/60)} minutes ago")
+    if _ago != None and _ago != 0:
+
+        print(
+            f"your last purchas {history[-1].buy_chicken} gr  chicken {_ago} hour ago")
+    if _ago == None:
+        print("you have not made a recent purchase\n")
+
+if any_choose == "4":
+
+    eat_ago = round(
+        (datetime.utcnow() - db.eat[-1].date).total_seconds()/3600)
+    if eat_ago == 0:
+        print(f"{db.eat[-1].total_chicken} gr chicken and {db.eat[-1].total_rice} gr rice {int((datetime.utcnow() - db.eat[-1].date).total_seconds()/60)} minute ago by {db.eat[-1].username} ")
+    else:
+        print(
+            f"{db.eat[-1].total_chicken} gr chicken  and {db.eat[-1].total_rice} gr rice {eat_ago} hour ago by {db.eat[-1].username} ")
+
+    contunie = input("would you like to see the details\n"
+                     "1 - Yes\n"
+                     "2 - NO\n")
+    if contunie == "1":
+        print("\nchicken quantities : \n")
+        for item in range(len(db.eat[-1].username)):
+            print(
+                f"{db.eat[-1].username[item] } -> {db.eat[-1].eat_chicken[item]} gr chicken ")
+        print("\nrice quantities : \n")
+        for item in range(len(db.eat[-1].username)):
+            print(
+                f"{db.eat[-1].username[item] } -> {db.eat[-1].eat_rice[item]} gr rice")
+if any_choose == "5":
+    sum_debts_dict = sum_debts(db)
+    buyer_list = []
+    are_you_sure = ""
+    for user in Users:
+        key = f"{Users[user]}-{any_user}"
+        if key in sum_debts_dict and sum_debts_dict[key] != 0:
+
+            from_currency = "TRY"
+
+            to_currency = "USD"
+
+            amount = sum_debts_dict[key]
+
+            response = requests.get(
+                f"https://api.frankfurter.app/latest?amount={amount}&from={from_currency}&to={to_currency}")
+
+            print(
+                f"buyer -> {Users[user]}- creditor ->{any_user} = {sum_debts_dict[key]} {from_currency} -> {response.json()['rates'][to_currency]} {to_currency} ")
+
+            buyer_list.append(Users[user])
+    contunie = input("would you like to pay\n"
+                     "1 - Yes with TRY\n"
+                     "2 - Yes with USD\n"
+                     "3 - NO\n")
+    if contunie == "1" or contunie == "2":
+        i = 0
+        for buyer in buyer_list:
+            print(f"{i}-{buyer}")
+            i += 1
+
+    if contunie == "3":
+        print("are you a pool 🤨🤨🤨")
+        exit(0)
+    buyer_choose = input("Who do you want to repay your debt to?\n")
+    key_buyer = f"{buyer_list[int(buyer_choose)]}-{any_user}"
+    if contunie == "1":
+        are_you_sure = input(f"Your debt is {sum_debts_dict[key_buyer] } TRY\n"
+
+                             "1 - Pay all\n"
+                             "2 - pay as you want\n"
+                             "3 - Cancel\n")
+    if contunie == "2":
+        from_currency = "TRY"
+
+        to_currency = "USD"
+
+        amount = sum_debts_dict[key_buyer]
+
+        response = requests.get(
+            f"https://api.frankfurter.app/latest?amount={amount}&from={from_currency}&to={to_currency}")
+
+        are_you_sure = input(f"Your debt is {response.json()['rates'][to_currency]} USD \n"
+
+                             "1 - Pay all\n"
+                             "2 - pay as you want\n"
+                             "3 - Cancel\n")
+
+    if are_you_sure == "1":
+        write_database(pay_off(db, any_user, buyer_list[int(
+            buyer_choose)], sum_debts_dict[key_buyer]))
+        print("🫴🫴🫴🫴")
+
+    if are_you_sure == "2" and contunie == "2":
+        from_currency = "USD"
+
+        to_currency = "TRY"
+
+        amount = input("how much do you want to pay?\n")
+
+        response = requests.get(
+            f"https://api.frankfurter.app/latest?amount={amount}&from={from_currency}&to={to_currency}")
+
+        write_database(pay_off(db, any_user, buyer_list[int(
+            buyer_choose)], float(response.json()['rates'][to_currency])))
+        print("🫴🫴🫴🫴")
+    if are_you_sure == "2" and contunie == "1":
+        per_debt = input("how much do you want to pay?\n")
+        write_database(pay_off(db, any_user, buyer_list[int(
+            buyer_choose)], float(per_debt)))
+        print("🫴🫴🫴🫴")
+    if are_you_sure == "3":
+        print("ne dedi ne dedi ✋✋✋")
